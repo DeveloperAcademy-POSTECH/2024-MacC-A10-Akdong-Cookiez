9e0a1df134fece9577c34d33c733b378c862b0e0
bono <shippingbok@gmail.com>
2024-11-09 03:20:11 +0900

    Refactor: 사용하지 않는 UILabel 제거

--- AKCOO/Scene/ExpenseScene/Controller/ExpenseSceneController.swift
+++ AKCOO/Scene/ExpenseScene/Controller/ExpenseSceneController.swift
@@ -12,12 +12,6 @@ class ExpenseSceneController: UIViewController {
   private let statsViewController: ExpenseStatsViewController
 
   // MARK: - Views
-  let label: UILabel = { // 화면 구분을 위한 임시생성
-    let label = UILabel()
-    label.text = "ExpenseSceneController"
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
   
   // MARK: - Initializers
   init(recordViewController: ExpenseRecordViewController, statsViewController: ExpenseStatsViewController) {
@@ -42,13 +36,11 @@ class ExpenseSceneController: UIViewController {
     addChild(recordViewController)
     view.addSubview(recordViewController.view)
     recordViewController.didMove(toParent: self)
-
-    view.addSubview(label)
   }
 
   private func setupConstraints() {
     recordViewController.view.translatesAutoresizingMaskIntoConstraints = false
-
+    
     NSLayoutConstraint.activate([
       recordViewController.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       recordViewController.view.centerYAnchor.constraint(equalTo: view.centerYAnchor)
