7de7d5fd1ccea1f9c149259bab374e1e5e4cb806
bono <shippingbok@gmail.com>
2024-11-08 21:31:18 +0900

    Setting: ExpenseScene 생성

---
+++ AKCOO/Scene/ExpenseScene/Controller/ExpenseStatsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExpenseStatsViewController.swift
+//  AKCOO
+//
+//  Created by 박혜운 on 11/6/24.
+//
+
+import UIKit
+
+class ExpenseStatsViewController: UIViewController {
+  private let expenseUseCase: ExpenseUseCase
+  weak var coordinator: ExpenseCoordinator?
+  
+  // MARK: - Properties
+  private let id: Int
+  
+  // MARK: - Initializers
+  init(countryId id: Int, expenseUseCase: ExpenseUseCase) {
+    self.id = id
+    self.expenseUseCase = expenseUseCase
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
