97e047ca9890fc9e8a3d5829e40ada4051f68f19
Seowoo <sc204016@gmail.com>
2024-11-10 00:06:44 +0900

    feat/#12 arrowRight asset 추가

--- AKCOO/Scene/TravelScene/Controller/TravelListSceneController.swift
+++ AKCOO/Scene/TravelScene/Controller/TravelListSceneController.swift
@@ -82,7 +82,7 @@ class TravelListSceneController: UIViewController {
   // MARK: - Private Methods
   private func initConfigure() {
     // useCase에서 데이터 가져와서 초기 세팅
-      self.travels.append(.init( flag: "🇹🇭", country: "캐나다", currency: .init(unitTitle: "바트", unit: 4), startDate: .now, endDate: .now.addingTimeInterval(3), budget: .init(total: 3000000)))
+      self.travels.append(.init( flag: "🇹🇭", country: "태국", currency: .init(unitTitle: "바트", unit: 4), startDate: .now, endDate: .now.addingTimeInterval(3), budget: .init(total: 3000000)))
       let info: [TravelCellInfo] = travels.map { ($0.flag, $0.country, "어쩌구기간") }
     travelTableView.setConfigure(info: info)
   }
