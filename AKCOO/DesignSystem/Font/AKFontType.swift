1afda8c55e290ec3219b7b95d57c291f7d211d2f
bono <shippingbok@gmail.com>
2024-11-20 21:25:24 +0900

    Fix: DesignSystem Font, Color 변경

--- AKCOO/DesignSystem/Font/AKFontType.swift
+++ AKCOO/DesignSystem/Font/AKFontType.swift
@@ -8,58 +8,35 @@
 import Foundation
 
 enum AKFontType {
-  case gmarketMedium32
-  case gmarketMedium30
-  case gmarketMedium24
-  case gmarketMedium16
+  case gmarketMedium12
+  case gmarketMedium13
   case gmarketMedium14
+  case gmarketMedium16
   
-  case gmarketLight16
-  case gmarketLight12
-  
-  case monoRegular32
-  
-  case monoMedium24
-  case monoMedium16
-  
-  case monoSemibold16
+  case gmarketBold16
+  case gmarketBold18
+  case gmarketBold20
   
   var customFont: String {
     switch self {
-    case .gmarketMedium32, .gmarketMedium30, .gmarketMedium24, .gmarketMedium16, .gmarketMedium14:
+    case .gmarketMedium12, .gmarketMedium13, .gmarketMedium14, .gmarketMedium16:
       return CustomFont.gmarketSansMedium.name
       
-    case .gmarketLight12, .gmarketLight16:
-      return CustomFont.gmarketSansLight.name
-      
-    case .monoRegular32:
-      return CustomFont.sfMonoRegular.name
-      
-    case .monoMedium16, .monoMedium24:
-      return CustomFont.sfMonoMedium.name
-      
-    case .monoSemibold16:
-      return CustomFont.sfMonoSemibold.name
+    case .gmarketBold16, .gmarketBold18, .gmarketBold20:
+      return CustomFont.gmarketSansBold.name
     }
   }
   
   var size: CGFloat {
     switch self {
-    case .gmarketMedium32: return 32
-    case .gmarketMedium30: return 30
-    case .gmarketMedium24: return 24
-    case .gmarketMedium16: return 16
-    case .gmarketMedium14: return 14
+    case .gmarketMedium12: 12
+    case .gmarketMedium13: 13
+    case .gmarketMedium14: 14
+    case .gmarketMedium16: 16
       
-    case .gmarketLight16:  return 16
-    case .gmarketLight12:  return 12
-      
-    case .monoRegular32:   return 32
-
-    case .monoMedium24:    return 24
-    case .monoMedium16:    return 16
-      
-    case .monoSemibold16:  return 16
+    case .gmarketBold16: 16
+    case .gmarketBold18: 18
+    case .gmarketBold20: 20
     }
   }
 }
