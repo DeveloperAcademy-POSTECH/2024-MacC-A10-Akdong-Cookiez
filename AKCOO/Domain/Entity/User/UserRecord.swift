16b50779adb75ffc32a4b840683058eeede9a5f9
Anjin Lee <anjin720jin@naver.com>
2024-11-21 22:18:45 +0900

    Feat: UserRecord에 나라 이름 추가

--- AKCOO/Domain/Entity/User/UserRecord.swift
+++ AKCOO/Domain/Entity/User/UserRecord.swift
@@ -10,10 +10,10 @@ import Foundation
 /// 사용자 지출 판단 기록 
 struct UserRecord {
   let date: Date
-  var currency: Currency
+  var country: CountryProfile
   var userQuestion: UserQuestion
   var userJudgment: JudgmentType
-  var externalJudgment: [String:JudgmentType]
+  var externalJudgment: [String: JudgmentType]
 }
 
 extension UserRecord: Standard {
