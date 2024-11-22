78642c9f97a82ef0ee8d524c35e67f3ab3b93d50
bono <shippingbok@gmail.com>
2024-11-22 18:47:40 +0900

    Style: BirdCaharacterImageType 생성

---
+++ AKCOO/Scene/Common/Type/BirdCharacterImageType.swift
@@ -0,0 +1,30 @@
+//
+//  BirdCharacterImageType.swift
+//  AKCOO
+//
+//  Created by 박혜운 on 11/20/24.
+//
+
+import UIKit
+
+enum BirdCharacterImageType {
+  case foriegn
+  case local
+  case previous
+  
+  var buying: UIImage {
+    switch self {
+    case .foriegn:  return UIImage(resource: .greenbirdpointL)
+    case .local:    return UIImage(resource: .redbirdpointL)
+    case .previous: return UIImage(resource: .yellowbirdpointL)
+    }
+  }
+  
+  var notBuying: UIImage {
+    switch self {
+    case .foriegn:  return UIImage(resource: .greenbirdpointR)
+    case .local:    return UIImage(resource: .redbirdpointR)
+    case .previous: return UIImage(resource: .yellowbirdpointR)
+    }
+  }
+}
