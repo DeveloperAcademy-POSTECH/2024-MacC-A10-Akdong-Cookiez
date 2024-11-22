b151ad4e5ce27fd97699e95ca8e5d7e8f5be22ab
bono <shippingbok@gmail.com>
2024-11-22 18:49:22 +0900

    Feat: BirdReactionCollectinoView 생성

--- AKCOO/Scene/JudgmentScene/View/Reaction/BirdReactionCollectionView.swift
+++ AKCOO/Scene/JudgmentScene/View/Reaction/BirdReactionCollectionView.swift
@@ -7,18 +7,20 @@ class BirdReactionCollectionView: UIView {
     layout.scrollDirection = .vertical
     layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
     layout.minimumLineSpacing = 30
+    layout.sectionInset = UIEdgeInsets(top: 13, left: 0, bottom: 60, right: 0)
     
     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
     collectionView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.backgroundColor = .blue
     collectionView.delegate = self
     collectionView.dataSource = self
     collectionView.register(BirdReactionCell.self, forCellWithReuseIdentifier: BirdReactionCell.identifier)
+    collectionView.alpha = 0
     return collectionView
   }()
   
   // MARK: - Properties
-  private var reactionResults: [BirdReactionTextView] = []
+  private var birdModels: [BirdModel] = []
+  private var cellHeights: [IndexPath: CGFloat] = [:]
   
   // MARK: - Initializers
   override init(frame: CGRect) {
@@ -48,39 +50,61 @@ class BirdReactionCollectionView: UIView {
   }
   
   // MARK: - Public Methods
-  func configure(with reactionResults: [BirdReactionTextView]) {
-    // TODO: - cell 재정의 후 올라가는 화면 전체적으로 수정
-    self.reactionResults = reactionResults
-    reactionResults.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+  func configure(with birdModels: [BirdModel]) {
+    self.birdModels = birdModels
     collectionView.reloadData()
   }
 }
 
 // MARK: - UICollectionViewDataSource
 extension BirdReactionCollectionView: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return reactionResults.count
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return birdModels.count
   }
   
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+  ) -> UICollectionViewCell {
     guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BirdReactionCell.identifier, for: indexPath) as? BirdReactionCell else {
       return UICollectionViewCell()
     }
-    cell.alpha = 0
-    cell.configure(with: reactionResults[indexPath.item])
     
+    let birdModel = birdModels[indexPath.item]
+    let birdImageType: BirdCharacterImageType = {
+      switch birdModel {
+      case is ForignBird: return .foriegn
+      case is LocalBird: return .local
+      case is PreviousDayBird: return .previous
+      default: fatalError("Unknown BirdModel type")
+      }
+    }()
+    
+    cell.configure(
+      name: birdModel.name,
+      opinion: birdModel.opinion,
+      detail: birdModel.detail,
+      buying: birdModel.judgment,
+      birdImageType: birdImageType
+    )
+    
+    let calculatedHeight = cell.cellHeight()
+    cellHeights[indexPath] = calculatedHeight
+    collectionView.alpha = 1
     // 초기 상태 설정 (셀을 화면 위로 이동)
-    cell.transform = CGAffineTransform(translationX: collectionView.frame.width*2, y: 0)
+    cell.transform = CGAffineTransform(translationX: birdModel.judgment ? collectionView.frame.width*2 : -collectionView.frame.width*2, y: 0)
     
     UIView.animate(
       withDuration: 0.4, // 좀 더 오래 지속되는 애니메이션
       delay: 0.3 * Double(indexPath.item), // 각 셀이 약간의 시간 차이를 두고 등장
       usingSpringWithDamping: 0.9, // 스프링 효과를 줄 때 사용 (0에 가까울수록 더 강한 스프링)
-      initialSpringVelocity: 0.5, // 초기 스프링 속도
+      initialSpringVelocity: 0.2, // 초기 스프링 속도
       options: [.curveEaseInOut],
       animations: {
         cell.transform = .identity // 원래 위치로 이동
-        cell.alpha = 1 // 투명도 정상화
       },
       completion: nil
     )
@@ -91,29 +115,57 @@ extension BirdReactionCollectionView: UICollectionViewDataSource {
 
 // MARK: - UICollectionViewDelegate
 extension BirdReactionCollectionView: UICollectionViewDelegate {
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    print("Cell at index \(indexPath.item) selected")
-    let selectedView = reactionResults[indexPath.item]
-    selectedView.extendingHeight()
-    //    collectionView.reloadItems(at: [indexPath])
+  func collectionView(
+    _ collectionView: UICollectionView,
+    didSelectItemAt indexPath: IndexPath
+  ) {
+    guard let selectedCell = collectionView.cellForItem(at: indexPath) as? BirdReactionCell
+    else { return }
+    selectedCell.tappedCell()
     collectionView.performBatchUpdates(nil, completion: nil)
-    
   }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension BirdReactionCollectionView: UICollectionViewDelegateFlowLayout {
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let width = collectionView.frame.width // Inset adjustment
-    let targetView = reactionResults[indexPath.item]
-    let targetHeight = targetView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
-    return CGSize(width: width, height: targetHeight)
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    sizeForItemAt indexPath: IndexPath
+  ) -> CGSize {
+    if let height = cellHeights[indexPath] {
+      return CGSize(width: collectionView.bounds.width, height: height)
+    } else {
+      // 셀을 직접 생성하여 높이 계산
+      if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BirdReactionCell.identifier, for: indexPath) as? BirdReactionCell {
+        let calculatedHeight = cell.cellHeight()
+        cellHeights[indexPath] = calculatedHeight
+        return CGSize(width: collectionView.bounds.width, height: calculatedHeight)
+      }
+      // 기본 높이 값 반환
+      return CGSize(width: collectionView.bounds.width, height: 100)
+    }
   }
 }
 
-// Preview for ReactionCollectionView
 #Preview {
   let preview = BirdReactionCollectionView()
-  preview.configure(with: [BirdReactionTextView(), BirdReactionTextView(), BirdReactionTextView()])
+  preview.configure(
+    with: [
+      LocalBird(
+        country: .init(
+          name: "대한민국",
+          currency: .init(
+            unitTitle: "원",
+            unit: 1
+          )
+        ),
+        judgment: .init(
+          userAmount: 30000, standards: [ ]
+        )
+      )
+    ]
+  )
+  
   return preview
 }
