1b9979e2790a1be8f0e5e47806348c3beb8157ad
Anjin Lee <anjin720jin@naver.com>
2024-11-25 00:02:51 +0900

    Feat: 한국/해외 새의 (사/말아) 판단 기준 추가

--- AKCOO/Domain/Entity/Judgmemt/CountryAverageJudgment.swift
+++ AKCOO/Domain/Entity/Judgmemt/CountryAverageJudgment.swift
@@ -11,7 +11,25 @@ struct CountryAverageJudgment: Judgment {
   var userAmount: Double
   var standards: [Item]
   var result: JudgmentType {
-    // TODO: - 조건 만들기
-    return .buying
+    guard let average = averageOfItems else { return .notBuying }
+    let isBelowOrEqualAverage = userAmount <= average
+    if isBelowOrEqualAverage {
+      return .buying
+    } else {
+      return .notBuying
+    }
+  }
+  
+  // 기준값 평균
+  private var averageOfItems: Double? {
+    // 기준이 비어있다면 nil 반환
+    guard standards.isEmpty == false else { return nil }
+    // 기준값들의 합
+    let total = standards.reduce(0.0) { (sum, item) in
+      sum + item.amount
+    }
+    
+    let average = total / Double(standards.count)
+    return average
   }
 }
