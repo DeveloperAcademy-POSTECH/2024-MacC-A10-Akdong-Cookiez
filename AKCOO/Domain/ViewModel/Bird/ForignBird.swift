bea1d677ce385709e6011bfb5297d74765c7f47e
Anjin Lee <anjin720jin@naver.com>
2024-11-25 02:37:15 +0900

    Feat: 해외짹 opinion(말풍선 title) 설정

--- AKCOO/Domain/ViewModel/Bird/ForignBird.swift
+++ AKCOO/Domain/ViewModel/Bird/ForignBird.swift
@@ -31,9 +31,36 @@ struct ForignBird: BirdModel {
   }
   
   var opinion: String {
-    // TODO: - 미정
+    let category = judgmentCriteria.standards.first?.category
+    
     switch birdReaction {
-    default: return "너무 비싼데..? 정말 필요해?"
+    case .strongYes:
+      if let category, ["숙박", "식당", "카페"].contains(category) {
+        // 카테고리가 숙박, 식당, 카페인 경우
+        return "뭘 고민해! 무조건 가야지"
+      } else {
+        // 카테고리가 기타이거나 없는 경우
+        return "뭘 고민해! 무조건 소비해"
+      }
+      
+    case .mediumYes:
+      return "이 가격이면 나쁘지 않아"
+      
+    case .weakYes:
+      return "현지에서도 저렴한 금액이야"
+      
+    case .weakNo:
+      return "음.. 조금 비싼데!?"
+      
+    case .mediumNo:
+      return "음.. 꽤 비싼 편이야"
+      
+    case .strongNo:
+      if let category {
+        return "너무 비싸! 다른 \(category) 찾아봐"
+      } else {
+        return "너무 비싸! 다른 걸 찾아봐"
+      }
     }
   }
   
