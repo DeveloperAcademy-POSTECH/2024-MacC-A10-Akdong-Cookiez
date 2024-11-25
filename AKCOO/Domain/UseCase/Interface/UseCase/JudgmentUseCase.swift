842ab9fc1016f567ce2b30f4a01350a2b3bc30c7
bono <shippingbok@gmail.com>
2024-11-26 01:34:24 +0900

    Feat: JudgmentEditView 생성 및 수정

--- AKCOO/Domain/UseCase/Interface/UseCase/JudgmentUseCase.swift
+++ AKCOO/Domain/UseCase/Interface/UseCase/JudgmentUseCase.swift
@@ -11,9 +11,9 @@ protocol JudgmentUseCase {
   /// 판단을 저장하는 메서드
   func save(record: UserRecord) -> Result<VoidResponse, Error>
   /// 화면에 필요한 정보를 반환하는 메서드
-  func getPaperModel() async -> Result<PaperModel, Error>
+  func getPaperModel() -> Result<PaperModel, Error>
   /// 나라 변경 후 필요한 정보를 반환하는 메서드
-  func getNewPaperModel(newCountryName selectedCountryName: String) async -> Result<PaperModel, Error>
+  func getNewPaperModel(newCountryName selectedCountryName: String) -> Result<PaperModel, Error>
   /// 특정 국가, 카테고리, 서브 카테고리에 대한 판정을 요청하는 메서드
   func getBirdsJudgment(userQuestion: UserQuestion) -> Result<[BirdModel], Error>
 }
