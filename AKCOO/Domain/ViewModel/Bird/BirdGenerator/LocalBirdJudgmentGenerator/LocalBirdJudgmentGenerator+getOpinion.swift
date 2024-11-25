0872704916b46f32106264974973f946df82b9a6
Anjin Lee <anjin720jin@naver.com>
2024-11-26 00:00:53 +0900

    Feat: 한국짹의 기준별 opinion 문구 설정

--- AKCOO/Domain/ViewModel/Bird/BirdGenerator/LocalBirdJudgmentGenerator/LocalBirdJudgmentGenerator+getOpinion.swift
+++ AKCOO/Domain/ViewModel/Bird/BirdGenerator/LocalBirdJudgmentGenerator/LocalBirdJudgmentGenerator+getOpinion.swift
@@ -9,6 +9,34 @@ import Foundation
 
 extension LocalBirdJudgmentGenerator {
   func getOpinion(judgmentCriteria: CountryAverageJudgment, reaction: BirdReaction) -> String {
-    return ""
+    let category = judgmentCriteria.standards.first?.category
+    
+    switch reaction {
+    case .strongYes:
+      return "한국에서는 볼 수 없는 가격이야"
+      
+    case .mediumYes:
+      return "한국에서도 저렴한 가격이야"
+      
+    case .weakYes:
+      return "안 사면 한국 가서 후회할걸?"
+      
+    case .weakNo:
+      return "한국보다 조금 비싼 편이야"
+      
+    case .mediumNo:
+      if let category {
+        return "차라리 한국에서 \(category) 어때?"
+      } else {
+        return "차라리 한국에서 소비해"
+      }
+      
+    case .strongNo:
+      if let category {
+        return "너무 비싸! 다른 \(category) 찾아봐"
+      } else {
+        return "너무 비싸! 다른 걸 찾아봐"
+      }
+    }
   }
 }
