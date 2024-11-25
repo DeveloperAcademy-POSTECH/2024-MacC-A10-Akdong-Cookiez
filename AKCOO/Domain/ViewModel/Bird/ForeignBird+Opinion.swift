b32d3a82febe865fb792f3593d588d25c841c628
Anjin Lee <anjin720jin@naver.com>
2024-11-25 19:15:15 +0900

    Feat: 해외잭 title 파일 분리

---
+++ AKCOO/Domain/ViewModel/Bird/ForeignBird+Opinion.swift
@@ -0,0 +1,44 @@
+//
+//  ForeignBird+Opinion.swift
+//  AKCOO
+//
+//  Created by Anjin on 11/25/24.
+//
+
+import Foundation
+
+struct BirdOpinionGenerator {
+  private func getOpinion(judgmentCriteria: CountryAverageJudgment, reaction: BirdReaction) -> String {
+    let category = judgmentCriteria.standards.first?.category
+    
+    switch reaction {
+    case .strongYes:
+      if let category, ["숙박", "식당", "카페"].contains(category) {
+        // 카테고리가 숙박, 식당, 카페인 경우
+        return "뭘 고민해! 무조건 가야지"
+      } else {
+        // 카테고리가 기타이거나 없는 경우
+        return "뭘 고민해! 무조건 소비해"
+      }
+      
+    case .mediumYes:
+      return "이 가격이면 나쁘지 않아"
+      
+    case .weakYes:
+      return "현지에서도 저렴한 금액이야"
+      
+    case .weakNo:
+      return "음.. 조금 비싼데!?"
+      
+    case .mediumNo:
+      return "음.. 꽤 비싼 편이야"
+      
+    case .strongNo:
+      if let category {
+        return "너무 비싸! 다른 \(category) 찾아봐"
+      } else {
+        return "너무 비싸! 다른 걸 찾아봐"
+      }
+    }
+  }
+}
--- AKCOO/Domain/ViewModel/Bird/ForignBird.swift
+++ AKCOO/Domain/ViewModel/Bird/ForignBird.swift
@@ -62,6 +62,7 @@ struct ForignBird: BirdModel {
       }
       
     // item이 3개 이상일 때
+    // FIXME: case 3... 을 추가하고 default는 별개로 두기
     default:
       guard sortedItems.count >= 3 else { return .mediumYes }
       
@@ -141,43 +142,6 @@ struct ForignBird: BirdModel {
   }
   
   var opinion: String {
-    let category = judgmentCriteria.standards.first?.category
-    
-    switch birdReaction {
-    case .strongYes:
-      if let category, ["숙박", "식당", "카페"].contains(category) {
-        // 카테고리가 숙박, 식당, 카페인 경우
-        return "뭘 고민해! 무조건 가야지"
-      } else {
-        // 카테고리가 기타이거나 없는 경우
-        return "뭘 고민해! 무조건 소비해"
-      }
-      
-    case .mediumYes:
-      return "이 가격이면 나쁘지 않아"
-      
-    case .weakYes:
-      return "현지에서도 저렴한 금액이야"
-      
-    case .weakNo:
-      return "음.. 조금 비싼데!?"
-      
-    case .mediumNo:
-      return "음.. 꽤 비싼 편이야"
-      
-    case .strongNo:
-      if let category {
-        return "너무 비싸! 다른 \(category) 찾아봐"
-      } else {
-        return "너무 비싸! 다른 걸 찾아봐"
-      }
-    }
-  }
-  
-  var detail: String {
-    // TODO: - 미정
-    switch birdReaction {
-    default: return "너무 비싼데..? 정말 필요해?"
-    }
+    ""
   }
 }
