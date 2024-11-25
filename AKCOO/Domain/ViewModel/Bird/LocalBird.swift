35102c83ef4378710b2fa3ab836a2595688f6152
Anjin Lee <anjin720jin@naver.com>
2024-11-26 00:35:07 +0900

    Feat: LocalBird의 reaction, opinion, detail을 generator에서 불러오기

--- AKCOO/Domain/ViewModel/Bird/LocalBird.swift
+++ AKCOO/Domain/ViewModel/Bird/LocalBird.swift
@@ -11,6 +11,7 @@ import Foundation
 struct LocalBird: BirdModel {
   private let country: CountryProfile
   var judgmentCriteria: CountryAverageJudgment
+  private let judgmentGenerator: BirdJudgmentGenerator
   
   init(
     country: CountryProfile,
@@ -18,28 +19,35 @@ struct LocalBird: BirdModel {
   ) {
     self.country = country
     self.judgmentCriteria = judgment
+    self.judgmentGenerator = LocalBirdJudgmentGenerator()
   }
   
   var criteriaName: String { return judgmentCriteria.name }
   var judgment: Bool { return judgmentCriteria.result == .buying }
   
-  private var birdReaction: BirdReaction {
-    return .mediumNo
-  }
-  
-  var name: String { return "대한민국 토박이" }
+  var name: String { return "한국 토박이" }
   var information: String { return "나는 한국 토박이야!" }
   
+  private var birdReaction: BirdReaction {
+    return judgmentGenerator
+      .getReaction(
+        judgmentCriteria: self.judgmentCriteria
+      )
+  }
+  
   var opinion: String {
-    switch birdReaction {
-    default: return "너무 비싼데..? 정말 필요해?"
-    }
+    return judgmentGenerator
+      .getOpinion(
+        judgmentCriteria: self.judgmentCriteria,
+        reaction: self.birdReaction
+      )
   }
   
   var detail: String {
-    // 선택된 카테고리와 서브 카테고리의 평균 금액을 기준으로 세부 설명 작성
-    switch birdReaction {
-    default: return "일반적인 베트남의 식당 가격보다\n약 40,000동 비싸요!\n하지만 캐쥬얼다이닝의 가격과 비교하면\n약 50,000동 저렴한 편이에요."
-    }
+    return judgmentGenerator
+      .getDetail(
+        country: self.country,
+        judgmentCriteria: self.judgmentCriteria
+      )
   }
 }
