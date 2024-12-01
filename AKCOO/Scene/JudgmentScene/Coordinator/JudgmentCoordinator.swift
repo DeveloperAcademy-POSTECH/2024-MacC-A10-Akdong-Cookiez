c1bc2fb0cd07cdb8108d3c66329f1590023639dd
bono <shippingbok@gmail.com>
2024-12-02 02:25:07 +0900

    Feat: JudgmentCoordinator 화면 삭제 로직 추가

--- AKCOO/Scene/JudgmentScene/Coordinator/JudgmentCoordinator.swift
+++ AKCOO/Scene/JudgmentScene/Coordinator/JudgmentCoordinator.swift
@@ -12,4 +12,5 @@ protocol JudgmentCoordinator: Coordinator {
   func completedJudgment(judgmentViewController: UIViewController)
   func startEditPaper(presenting: JudgmentEditViewControllerDelegate, paperModel: PaperModel, selectedCategory: String, userAmount: String)
   func completedEditPaper(editViewController: UIViewController)
+  func dismiss()
 }
