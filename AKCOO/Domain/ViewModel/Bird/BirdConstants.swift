ed1baa702d5ae1881e15fcde5e6048368e7f1bea
Anjin Lee <anjin720jin@naver.com>
2024-12-02 00:15:51 +0900

    Feat: LocalBirdOpinion Constant 추가

--- AKCOO/Domain/ViewModel/Bird/BirdConstants.swift
+++ AKCOO/Domain/ViewModel/Bird/BirdConstants.swift
@@ -15,8 +15,24 @@ struct BirdConstants {
     static let weakYes: String = "현지에서도 저렴한 가격이야"
     static let weakNo: String = "음.. 조금 비싼데!?"
     static let mediumNo: String = "음.. 꽤 비싼 편이야"
-    static let strongNo: String = "너무 비싸! 다른 걸 찾아봐"
+    
     // 사용 시, 카테고리 문자열 추가 필요
     static let strongNoWithCategory: String = "너무 비싸! 다른 %@ 찾아봐"
+    static let strongNo: String = "너무 비싸! 다른 걸 찾아봐"
+  }
+  
+  struct LocalBirdOpinion {
+    static let strongYes: String = "한국에서는 볼 수 없는 가격이야"
+    static let mediumYes: String = "한국에서도 저렴한 가격이야"
+    static let weakYes: String = "한국이랑 비슷한 편이야"
+    static let weakNo: String = "한국보다 조금 비싼 편이야"
+    
+    // 사용 시, 카테고리 문자열 추가 필요
+    static let mediumNoWithCategory: String = "차라리 한국에서 %@ 어때?"
+    static let mediumNo: String = "차라리 한국에서 소비해"
+    
+    // 사용 시, 카테고리 문자열 추가 필요
+    static let strongNoWithCategory: String = "너무 비싸! 다른 %@ 찾아봐"
+    static let strongNo: String = "너무 비싸! 다른 걸 찾아봐"
   }
 }
