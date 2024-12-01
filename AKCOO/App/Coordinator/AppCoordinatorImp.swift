ca425e2a3cca391ddf68a9d7b640262f02712eae
bono <shippingbok@gmail.com>
2024-12-02 02:26:49 +0900

    Feat: AppCoordinator 변경

--- AKCOO/App/Coordinator/AppCoordinatorImp.swift
+++ AKCOO/App/Coordinator/AppCoordinatorImp.swift
@@ -21,6 +21,12 @@ final class AppCoordinatorImp: AppCoordinator {
   // App 폴더 내 최상위 Coordinator인 AppCoordinator이므로 직접 주입합니다
   func start() {
     let judgmentUseCase = JudgmentUseCaseImp(judgmentRepository: JudgmentRepositoryMock(), recordRepository: RecordRepositoryImp())
+    
+    let guideUseCase = GuideUseCaseMock()
+    let userInfoUseCase = UserInfoUseCaseMock()
+    let guideFactory = GuideFactoryImp(useCase: guideUseCase)
+    let userInfoFactory = UserInfoFactoryImp(useCase: userInfoUseCase)
+    
     let judgmentReadyFactory = JudgmentReadyFactoryImp(useCase: judgmentUseCase)
     let judgmentCompletedFactory = JudgmentCompletedFactoryImp(useCase: judgmentUseCase)
     let judgmentEditFactory = JudgmentEditFactoryImp(useCase: judgmentUseCase)
@@ -32,7 +38,14 @@ final class AppCoordinatorImp: AppCoordinator {
       judgmentEditFactory: judgmentEditFactory
     )
     
-    childCoordinators.append(judgmentCoordinator)
-    judgmentCoordinator.start()
+    let guideCoordinator = GuideCoordinatorImp(
+      navigationController,
+      judgmentCoordinator: judgmentCoordinator,
+      guideFactory: guideFactory,
+      userInfoFactory: userInfoFactory
+    )
+    
+    childCoordinators.append(guideCoordinator)
+    guideCoordinator.start()
   }
 }
