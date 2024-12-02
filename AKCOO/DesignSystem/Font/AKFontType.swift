72cccaaae8a4396d38076343cbf69920c2a109a2
Anjin Lee <anjin720jin@naver.com>
2024-12-03 04:16:39 +0900

    Feat: 필요한 폰트 추가

--- AKCOO/DesignSystem/Font/AKFontType.swift
+++ AKCOO/DesignSystem/Font/AKFontType.swift
@@ -15,6 +15,7 @@ enum AKFontType {
   case gmarketMedium24
   case gmarketMedium30
   
+  case gmarketBold12
   case gmarketBold13
   case gmarketBold14
   case gmarketBold16
@@ -27,7 +28,7 @@ enum AKFontType {
     case .gmarketMedium12, .gmarketMedium13, .gmarketMedium14, .gmarketMedium16, .gmarketMedium24, .gmarketMedium30:
       return CustomFont.gmarketSansMedium.name
       
-    case .gmarketBold13, .gmarketBold14, .gmarketBold16, .gmarketBold18, .gmarketBold20, .gmarketBold30:
+    case .gmarketBold12, .gmarketBold13, .gmarketBold14, .gmarketBold16, .gmarketBold18, .gmarketBold20, .gmarketBold30:
       return CustomFont.gmarketSansBold.name
     }
   }
@@ -40,7 +41,8 @@ enum AKFontType {
     case .gmarketMedium16: 16
     case .gmarketMedium24: 24
     case .gmarketMedium30: 30
-      
+    
+    case .gmarketBold12: 12
     case .gmarketBold13: 13
     case .gmarketBold14: 14
     case .gmarketBold16: 16
