0f2304d2958ad405840182fba0524c34c797f6ca
Anjin Lee <anjin720jin@naver.com>
2024-12-03 03:02:51 +0900

    Feat: 판단 유형에서 기록이 없을 때 문구 및 Row 변경

--- AKCOO/Scene/UserInfoScene/View/UserInfoRecordsView.swift
+++ AKCOO/Scene/UserInfoScene/View/UserInfoRecordsView.swift
@@ -30,11 +30,23 @@ struct UserInfoRecordsView: View {
       
       LazyVStack(spacing: 10) {
         if viewModel.userRecords.isEmpty {
-          HStack {
-            Spacer()
-            Text("아직 지출 판단 기록이 없어요")
-              .multilineTextAlignment(.center)
-            Spacer()
+          VStack(spacing: 10) {
+            HStack {
+              Text("아직 기록이 없어요")
+              Spacer()
+            }
+            .padding(.horizontal, 8)
+            
+            Line()
+              .stroke(
+                style: StrokeStyle(
+                  lineWidth: 0.4,       // 선 두께
+                  dash: [4, 2],         // 대시 패턴: [대시 길이, 간격]
+                  dashPhase: 0          // 대시 시작 위치
+                )
+              )
+              .foregroundColor(.black)  // 선 색상
+              .frame(height: 1)         // 선 높이 조정
           }
         } else {
           ForEach(viewModel.userRecords, id: \.id) { record in
