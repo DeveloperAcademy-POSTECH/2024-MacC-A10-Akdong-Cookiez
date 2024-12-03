036252ca25b93f461d486667b990bb3a7893eadd
Anjin Lee <anjin720jin@naver.com>
2024-12-03 20:33:15 +0900

    Feat: 지출 판단 기록에서 Omark 이미지 변경

--- AKCOO/Scene/UserInfoScene/View/UserInfoRecordsView.swift
+++ AKCOO/Scene/UserInfoScene/View/UserInfoRecordsView.swift
@@ -56,8 +56,10 @@ struct UserInfoRecordsView: View {
               HStack(spacing: 8) {
                 Text(record.userQuestion.category)
                 Spacer()
-                Text("\(record.userQuestion.amount.formattedWithCommas(maxDecimalPlaces: 0))\(record.userQuestion.country.currency.unitTitle)")
-                Image(record.userJudgment == .buying ? .confettiOmark : .confettiXmark)
+                
+                let place: Int = record.userQuestion.country.name == "베트남" ? 0 : 2
+                Text("\(record.userQuestion.amount.formattedWithCommas(maxDecimalPlaces: place)) \(record.userQuestion.country.currency.unitTitle)")
+                Image(record.userJudgment == .buying ? .userRecordOmark : .confettiXmark)
                   .resizable()
                   .frame(width: 16, height: 16)
               }
